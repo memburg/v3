739dbf3ba0949c37db51e45dfdb667d609cc0665
Ramb Memburg <46289413+memburg@users.noreply.github.com>
2020-12-14 18:02:16 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -19,9 +19,9 @@ const loop = () => {
     const hours = date.getHours()
     const minutes = date.getMinutes()
 
-    if (os.includes('Windows')) quote.innerText = `Using Windows? Why? Switch to Linux!`
-    else if (os.includes('Linux')) quote.innerText = `Linux rocks!`
-    else if (os.includes('Mac')) quote.innerText = `You are using a Mac, interesting...`
+    if (os.includes('Windows')) quote.innerText = 'Using Windows? Why? Switch to Linux!'
+    else if (os.includes('Linux')) quote.innerText = 'Linux rocks!'
+    else if (os.includes('Mac')) quote.innerText = 'You are using a Mac, interesting...'
 
     // This phrases are more important that the OS ones
     if (hours === 15) quote.innerText = `It is ${hours}:${minutes < 10 ? "0" + minutes : minutes}, let's go for a cup of tea!`
@@ -30,16 +30,16 @@ const loop = () => {
     // Filters and colour changing
     if (hours <= 5) {
         backgroundFilter.style.backgroundColor = hexEvening
-        backgroundImage.style.filter = "saturate(0.3) brightness(0.3) contrast(1.5) blur(3px)"
+        backgroundImage.style.filter = 'saturate(0.3) brightness(0.3) contrast(1.5) blur(3px)'
     } else if (hours <= 12) {
         backgroundFilter.style.backgroundColor = hexMorning
-        backgroundImage.style.filter = "saturate(0.8) brightness(1) contrast(1) blur(5px)"
+        backgroundImage.style.filter = 'saturate(0.8) brightness(1) contrast(1) blur(5px)'
     } else if (hours <= 19) {
         backgroundFilter.style.backgroundColor = hexAfternoon
-        backgroundImage.style.filter = "saturate(0.8) brightness(1) contrast(1) blur(5px)"
+        backgroundImage.style.filter = 'saturate(0.8) brightness(1) contrast(1) blur(5px)'
     } else {
         backgroundFilter.style.backgroundColor = hexEvening
-        backgroundImage.style.filter = "saturate(0.3) brightness(0.3) contrast(1.5) blur(3px)"
+        backgroundImage.style.filter = 'saturate(0.3) brightness(0.3) contrast(1.5) blur(3px)'
     }
 }
 
